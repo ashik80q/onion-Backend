56ec6345842e1df825c5621ca1d52dec7512adc2
Ashik khandakar <ashikbpdb@gmail.com>
2020-06-19 22:07:38 +0600

    Project end

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ const uri = process.env.DB_PATH;
 
  let client = new MongoClient(uri, { useNewUrlParser: true });
 
-const user = ['Ashik', 'Khandakar', 'Rofik', 'Tamime','Al-amin']
-
 
 app.get('/foods', (req,res) =>{
      client = new MongoClient(uri, { useNewUrlParser: true });
@@ -42,12 +40,88 @@ app.get('/foods', (req,res) =>{
       });
 });
 
-app.get('/users/:id', (req, res) =>{
 
+// get orders
+
+app.get('/orders', (req,res) =>{
+    client = new MongoClient(uri, { useNewUrlParser: true });
+   client.connect(err => {
+       const collection = client.db("onionDataStore").collection("orders");
+           
+           collection.find().toArray((err, documents) =>{
+               
+               if(err){
+                   console.log(err);
+                   res.status(500).send({massage:err});
+                   
+               }else{
+                   res.send(documents [0]);
+               }
+               
+           });
+     
+         
+       client.close();
+     });
+});
+
+
+
+app.get('/food/:id', (req, res) =>{
+    
     const id = req.params.id;
-    const name = user[id]
-    res.send({id, name});
+
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onionDataStore").collection("foods");
+        collection.find({id}).toArray((err, documents)=>{
+    
+            if(err){
+                console.log(err);
+                res.status(500).send({massage:err});
+                
+            }
+            else{
+               
+                res.send(documents [0]);
+            }
+            
+            
+        });
+          
+        client.close();
+      });
 })
+
+
+app.post('/getFoodId', (req, res) =>{
+    
+    const id = req.params.id;
+    const foodId = req.body;
+   console.log(foodId);
+   
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onionDataStore").collection("foods");
+        collection.find({id:{$in: foodId}}).toArray((err, documents)=>{
+    
+            if(err){
+                console.log(err);
+                res.status(500).send({massage:err});
+                
+            }
+            else{
+               
+                res.send(documents);
+            }
+            
+            
+        });
+          
+        client.close();
+      });
+})
+
 // Post 
 
 app.post('/addFood', (req, res) =>{
@@ -58,7 +132,7 @@ app.post('/addFood', (req, res) =>{
     client.connect(err => {
         const collection = client.db("onionDataStore").collection("foods");
             
-            collection.insertOne(food,(err, result) =>{
+            collection.insert(food,(err, result) =>{
                 
                 if(err){
                     console.log(err);
@@ -75,5 +149,32 @@ app.post('/addFood', (req, res) =>{
       });
 });
 
+
+app.post('/orderDetails', (req, res) =>{
+    const orderDetail = req.body;
+    orderDetail.orderTime = new Date();
+   console.log(orderDetail );
+   
+    client = new MongoClient(uri, { useNewUrlParser: true });
+
+    client.connect(err => {
+        const collection = client.db("onionDataStore").collection("orders");
+            
+            collection.insertOne(orderDetail,(err, result) =>{
+                
+                if(err){
+                    console.log(err);
+                    res.status(500).send({massage:err});
+                    
+                }else{
+                    res.send(result.ops[0]);
+                }
+                
+            });
+      
+          
+        client.close();
+      });
+});
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log('Listening to  port 8000'))
